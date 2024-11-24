bd509e6698794a36549fad458048c47bb2e7a9e7
OlegMoshkovich <oleg.mosh@gmail.com>
2024-11-24 10:07:27 +0100

    add loader

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box,Button, Typography, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+import { Box,Button, Typography, Dialog, DialogTitle, DialogContent, DialogActions, CircularProgress } from '@mui/material';
 import { useState } from 'react';
 // import LoadingScreen from './LoadingScreen'; // Import the LoadingScreen component
 import { validatePrefabElement } from './services/validation';
@@ -89,76 +89,94 @@ export default function Home() {
 
   return (
     <>
-      <Box
-        sx={{
-          width: '100dvw',
-          overflow: 'auto',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: 'white',
-          minHeight: '90dvh',
-          backgroundImage: 'radial-gradient(lightgray 1px, transparent 1px)',
-          backgroundSize: '30px 30px',
-        }}
-      >
-        {!serverResponse ? ( // Display initial content if no server response
-        <Box sx={{ display: 'flex', flexDirection:'column', alignItems: 'center', mt: 2, width: '100%' }}>
-          <Typography variant="overline" sx={{ color:'black', marginBottom:'60px', width:'600px', textAlign:'center', lineHeight:'1.5'}}>
-            Parser is the project created during Open source construction hackathon in Munich.
+      {loading ? (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: '100vh',
+            backgroundColor: 'white',
+          }}
+        >
+          <CircularProgress /> {/* Or any other loading indicator */}
+          <Typography variant="h6" sx={{ mt: 2 }}>
+            Loading...
           </Typography>
-          <input
-            id="fileInput"
-            type="file"
-            accept=".pdf"
-            style={{ display: 'none' }}
-            onChange={handleFileChange}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{
-              backgroundColor: 'black',
-              color: 'white',
-              borderRadius: '30px',
-              mb: 2
-            }}
-            onClick={handleButtonClick}
-          >
-            Upload PDF
-          </Button>
-
-          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 2, width: '100%' }}>
-            <Typography variant="caption" sx={{ color: 'black', marginTop: '0px' }}>
-              Uploaded pdf is processed using LLM and matched with the library of MOD components
-            </Typography>
-          </Box>
         </Box>
-        ) : ( // Display server response and validate button if response is received
-          <Box sx={{ display: 'flex', flexDirection:'column', alignItems: 'center', mt: 2, width: '100%', borderRadius:'10px', p:2 }}>
-            <Box sx={{ height: '400px', overflow: 'auto', flexGrow: 1, border:'1px solid black',  }}>
-              <Typography variant="body2" sx={{ color: 'black', textAlign: 'center', whiteSpace: 'pre-wrap' }}>
-                {JSON.stringify(serverResponse, null, 2)}
-              </Typography>
-            </Box>
+      ) : (
+        <Box
+          sx={{
+            width: '100dvw',
+            overflow: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'white',
+            minHeight: '90dvh',
+            backgroundImage: 'radial-gradient(lightgray 1px, transparent 1px)',
+            backgroundSize: '30px 30px',
+          }}
+        >
+          {!serverResponse ? ( // Display initial content if no server response
+          <Box sx={{ display: 'flex', flexDirection:'column', alignItems: 'center', mt: 2, width: '100%' }}>
+            <Typography variant="overline" sx={{ color:'black', marginBottom:'60px', width:'600px', textAlign:'center', lineHeight:'1.5'}}>
+              Parser is the project created during Open source construction hackathon in Munich.
+            </Typography>
+            <input
+              id="fileInput"
+              type="file"
+              accept=".pdf"
+              style={{ display: 'none' }}
+              onChange={handleFileChange}
+            />
             <Button
               variant="contained"
               color="primary"
-              onClick={handleValidation}
               sx={{
                 backgroundColor: 'black',
                 color: 'white',
                 borderRadius: '30px',
-                mb: 2,  // Add margin bottom of 2 units (16px)
-                mr: 2   // Add margin right of 2 units (16px) for spacing
+                mb: 2
               }}
+              onClick={handleButtonClick}
             >
-              Validate
+              Upload PDF
             </Button>
+
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 2, width: '100%' }}>
+              <Typography variant="caption" sx={{ color: 'black', marginTop: '0px' }}>
+                Uploaded pdf is processed using LLM and matched with the library of MOD components
+              </Typography>
+            </Box>
           </Box>
-        )}
-      </Box>
+          ) : ( // Display server response and validate button if response is received
+            <Box sx={{ display: 'flex', flexDirection:'column', alignItems: 'center', mt: 2, width: '100%', borderRadius:'10px', p:2 }}>
+              <Box sx={{ height: '400px', overflow: 'auto', flexGrow: 1, border:'1px solid black',  }}>
+                <Typography variant="body2" sx={{ color: 'black', textAlign: 'center', whiteSpace: 'pre-wrap' }}>
+                  {JSON.stringify(serverResponse, null, 2)}
+                </Typography>
+              </Box>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleValidation}
+                sx={{
+                  backgroundColor: 'black',
+                  color: 'white',
+                  borderRadius: '30px',
+                  mb: 2,  // Add margin bottom of 2 units (16px)
+                  mr: 2   // Add margin right of 2 units (16px) for spacing
+                }}
+              >
+                Validate
+              </Button>
+            </Box>
+          )}
+        </Box>
+      )}
       <Box sx={{ height: '60px', marginTop: '60px' }}>
         {loading && ( // Conditionally render the loading spinner
           <Button
